e2fedc3e9ed4bcbf1932962d8dd03db8714a81a4
Tanaji <tanajilondhe29@gmail.com>
2016-10-15 07:36:15 +0530

    session two

---
+++ src/com/acadgild/session/two/TestEmployee.java
@@ -0,0 +1,11 @@
+package com.acadgild.session.two;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee e = new Employee(1, "Tanaji");
+		
+		System.out.println(e);
+	}
+}
